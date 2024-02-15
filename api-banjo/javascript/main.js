703339b121fa3f04115e9030102783f6e5eb9eb2
Ab-Amin <abourrabiaamin@gmail.com>
2024-02-15 09:10:53 +0100

    update

--- api-banjo/javascript/main.js
+++ api-banjo/javascript/main.js
@@ -8,10 +8,13 @@ function banjoProfiles() {
   .then(response => response.json())
   .then(data => {
     console.log(data);
+    // i, pour numéroter chaque personnes et pouvoir afficher qui on veux plus tard  
+    // Aurait pu aussi utiliser les 'id' qui sont deja dans le tableau pour chaque personnes 
     data.users.forEach(function(user, i) {
-      profiles.innerHTML += `<img class="imgProfiles" src="${user.image}" data-index=${i} alt="">`
+      profiles.innerHTML += `<img class="imgProfiles" src="${user.image}" data-index=${i} title="${user.name}" alt="">`
     })
 
+    // Fonction qui affichera les infos d'un utilisateur
     function banjoProfileInfo(nbr) {
       profile.innerHTML = `
       <img src="${data.users[nbr].image}" alt="">
@@ -21,6 +24,7 @@ function banjoProfiles() {
       `
     }
 
+
     addEventListener('click', function(e) {
       if (e.target.classList.contains('imgProfiles')) {
         dataIndex = e.target.getAttribute('data-index');
@@ -28,6 +32,12 @@ function banjoProfiles() {
         console.log('click');
     
         banjoProfileInfo(dataIndex)
+
+        // Si j'avais use id au lieu de data index :
+        // Au lieu de data-index=${i} dans profiles.innerHTML ==> data-userid="${user.id}"
+        // Et appeler banjoProfileInfo comme ça :
+        // banjoProfileInfo(data.users[e.target.getAttribute('data-userid') - 1])
+        // - 1 car les id dans tableau commence par 1 et non par 0
       }
 
     });
